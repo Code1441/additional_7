7b6bf4c2fea28d3f3c70104312865b3f3a61875a
TatsianaGelahova <tgelahova99@gmail.com>
2018-03-02 21:58:05 +0200

    fix: add function copyArray

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-module.exports = function solveSudoku(matrix) {
-   
+module.exports =  function solveSudoku(matrix) {
+
     function getPossibilities(row,cell){ //возвращает возможные варианты для вставки в ячейку
-        if(matrix[row][cell] !='0') return false;
+        if(matrix[row][cell] !==0) return false;
         var result = [1,2,3,4,5,6,7,8,9];
 
         result = result.filter(function(number){ // проверка по горизонтали на повторяющиеся цифры
-            if (matrix[row].indexOf(number)!= -1) return false;
+            if (matrix[row].indexOf(number)!== -1) return false;
             else return true;
         });
         for(var i = 0; i < 9; i++){ //проверка по вертикали
             var index = result.indexOf(matrix[i][cell]);
-            if (index!=-1) result.splice(index,1);
+            if (index!==-1) result.splice(index,1);
         }
 
         //проверка на значения в квадрате 3*3
@@ -19,9 +19,17 @@ module.exports = function solveSudoku(matrix) {
         for(var i = rowBegin; i<rowBegin+3; i++)
             for(var j = cellBegin; j< cellBegin+3; j++){
                 var index = result.indexOf(matrix[i][j]);
-                if (index != -1) result.splice(index,1);
+                if (index !== -1) result.splice(index,1);
             }
 
+        return result;
+    }
+
+    function copyArray(array){
+        var result = [];
+        for(var i = 0; i < array.length; i++){
+            result[i] = array[i].slice(0);
+        }
         return result;
     }
 
@@ -34,11 +42,11 @@ module.exports = function solveSudoku(matrix) {
             for (var i = 0; i < 9; i++) {
                 for (var j = 0; j < 9; j++) {
                     possibleCells = getPossibilities(i, j);
-                    if (possibleCells == false) continue; // если значение у ячейки уже есть
+                    if (possibleCells === false) continue; // если значение у ячейки уже есть
 
                     if (possibleCells.length === 0) throw new Error('No moves');
 
-                    if (possibleCells.length == 1) { //если только одно значение, то оно и будет окончательным
+                    if (possibleCells.length === 1) { //если только одно значение, то оно и будет окончательным
                         matrix[i][j] = possibleCells[0];
                         changed = true;
                     }
@@ -51,48 +59,48 @@ module.exports = function solveSudoku(matrix) {
     function isFullFilled() { // проверяет заполнено ли судоку
         for(var i = 0; i < 9;i++)
             for(var j = 0; j < 9; j++){
-                if(matrix[i][j]=='0') return false;
+                if(matrix[i][j]===0) return false;
             }
-            return true;
+        return true;
     }
 
- function solution(){
- 
-     try{
-         simpleFill();
-     } catch (error){
-         return false;
-     }
+    function solution(){
 
-     if( isFullFilled() ) return true;
+        try{
+            simpleFill();
+        } catch (error){
+            return false;
+        }
 
-     var raw, cell;
-     for(var i = 0; i < 9; i++){
-         for(var j = 0; j < 9; j++){
-             if(matrix[i][j]=='0') {
-                 raw = i;
-                 cell = j;
-                 break;
-             }
-         }
-     }
+        if( isFullFilled() ) return true;
 
-     var possibleCells = getPossibilities(raw,cell);
-     var bufferMatrix, result;
-     for(var i = 0; i < possibleCells.length; i++){
-         bufferMatrix = matrix.slice(0);
-         matrix[raw][cell] = possibleCells[i];
+        var raw, cell;
+        for(var i = 0; i < 9; i++){
+            for(var j = 0; j < 9; j++){
+                if(matrix[i][j]===0) {
+                    raw = i;
+                    cell = j;
+                    break;
+                }
+            }
+        }
 
-         result = solution();
-         if(result) return true;
-         else{
-             matrix = bufferMatrix.slice(0);
-         }
-     }
-     return false;
+        var possibleCells = getPossibilities(raw,cell);
+        var bufferMatrix, result;
+        for( i = 0; i < possibleCells.length; i++){
+            bufferMatrix = copyArray(matrix);
+            matrix[raw][cell] = possibleCells[i];
 
- }
- solution();
- return matrix;
+            result = solution();
+            if(result === true) return true;
+            else{
+                matrix = copyArray(bufferMatrix);
+            }
+        }
+        return false;
+
+    }
+    solution();
+    return matrix;
 
 }
